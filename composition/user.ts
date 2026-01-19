70dff7b210e9b1314ae213d488afbc3aa80e98de
Your Name <APeramaki@users.noreply.github.com>
2026-01-19 19:53:28 +0200

    added in commented InMemoryUserRepo for easy changing

--- composition/user.ts
+++ composition/user.ts
@@ -2,7 +2,9 @@ import CreateUser from "../application/user/CreateUser";
 // import InMemoryUserRepository from "../infrastructure/InMemory/UserRepository";
 import postgresUserRepository from "../infrastructure/postgres/UserRepository";
 import { pool } from "../infrastructure/postgres/db";
+
 module.exports = function createUserUseCases() {
+  // const userRepo = new InMemoryUserRepository();
   const userRepo = new postgresUserRepository(pool);
 
   return {
