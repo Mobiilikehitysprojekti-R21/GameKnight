817f99bf727057628747b230621f3abe3a593887
Your Name <APeramaki@users.noreply.github.com>
2026-01-27 16:46:49 +0200

    WIP auth

--- interfaces/http/routes/boardgames.ts
+++ interfaces/http/routes/boardgames.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import FindBoardgame from "../../../application/boardgame/FindBoardGame";
 import findBoardGameController from "../controllers/findBoardGameController";
+const { requiresAuth } = require("express-openid-connect");
 
 export interface BoardGameRoutesDeps {
-    findBoardGame: FindBoardgame;
+  findBoardGame: FindBoardgame;
 }
 
-export default function userRoutes({ findBoardGame }: BoardGameRoutesDeps): Router {
+export default function userRoutes({
+  findBoardGame,
+}: BoardGameRoutesDeps): Router {
   const router = Router();
-  
-  router.get("/", findBoardGameController(findBoardGame));
+
+  router.get("/", requiresAuth(), findBoardGameController(findBoardGame));
 
   return router;
 }
--- interfaces/http/server.ts
+++ interfaces/http/server.ts
@@ -5,20 +5,38 @@ import CreateUser from "../../application/user/CreateUser";
 import FindBoardGame from "../../application/boardgame/FindBoardGame";
 import boardGameRouter from "./routes/boardgames";
 import ValidateNickname from "../../application/user/ValidateNickname";
+const { auth } = require("express-openid-connect");
+
+const authConfig = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "a long, randomly-generated string stored in env",
+  baseURL: "http://localhost:3000/",
+  clientID: "GiR9CnNp3tEtfBwin2V1C7ineiqaogD3",
+  issuerBaseURL: "https://gameknight.eu.auth0.com",
+};
 
 export interface HttpServerDeps {
   createUser: CreateUser;
-  validateNickname: ValidateNickname
+  validateNickname: ValidateNickname;
   findBoardGame: FindBoardGame;
 }
 
 export default function createHttpServer(deps: HttpServerDeps): Express {
   const app = express();
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  app.use(auth(authConfig));
+
   app.use(cors());
   app.use(express.json());
 
   app.use("/users", userRoutes(deps));
-  app.use("/boardgames", boardGameRouter(deps))
+  app.use("/boardgames", boardGameRouter(deps));
+
+  // req.isAuthenticated is provided from the auth router
+  app.get("/", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+  });
 
   return app;
 }
