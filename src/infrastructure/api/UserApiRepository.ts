f7566347a9f262fafdf945faa9d9da0254c5bdb5
Atte Perämäki <APeramaki@users.noreply.github.com>
2026-02-08 16:29:52 +0200

    Fix merge errors (#40)
    * Fix merge errors
    
    * chore: update architecture diagram
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/infrastructure/api/UserApiRepository.ts
+++ src/infrastructure/api/UserApiRepository.ts
@@ -1,92 +1,94 @@
-import { UserRepository } from "../../domain/repositories/UserRepository";
-import axios from "axios";
-import { User } from "../../domain/entities/User";
-import Constants from "expo-constants";
+import { UserRepository } from '../../domain/repositories/UserRepository';
+import axios from 'axios';
+import { User } from '../../domain/entities/User';
+import Constants from 'expo-constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import type { Location } from "../../domain/entities/Location";
+import type { Location } from '../../domain/entities/Location';
 
 /*
     - UserApiRepository handles HTTP requests to the backend
     - Implements the UserRepository interface from the domain layer
 */
 
-
 export class UserApiRepository implements UserRepository {
+  private apiUrl = Constants.expoConfig?.extra?.API_URL;
 
-    private apiUrl = Constants.expoConfig?.extra?.API_URL
+  // Checks if nickname is available
+  async validateNickname(nickname: string): Promise<boolean> {
+    // Debugging...
+    console.log('userapirepo: validoidaan nickia');
+    console.log('apiURL: ', this.apiUrl);
+    console.log('nicki: ', nickname);
 
-    // Checks if nickname is available
-    async validateNickname(nickname: string): Promise<boolean> {
-        // Debugging...
-        console.log("userapirepo: validoidaan nickia")
-        console.log('apiURL: ', this.apiUrl)
-        console.log('nicki: ', nickname)
+    try {
+      //POST request to backend
+      const response = await axios.post(
+        `${this.apiUrl}/users/validateNickname`,
+        { nickname }
+      );
 
-        try {
-            //POST request to backend
-            const response = await axios.post(`${this.apiUrl}/users/validateNickname`, { nickname })
-
-            console.log(response.data)  // debugging...
-            // return true, if nickname is available
-            // if nickname is unavailable -> backend returns false -> false === true --> return false
-            return response.data === true
-        } catch (e) {
-            console.error("axios error:", e)
-            throw e
-        }
+      console.log(response.data); // debugging...
+      // return true, if nickname is available
+      // if nickname is unavailable -> backend returns false -> false === true --> return false
+      return response.data === true;
+    } catch (e) {
+      console.error('axios error:', e);
+      throw e;
     }
+  }
 
-    // Sign up user and store in the database
-    async signUp(user: User): Promise<void> {
-        console.log("rekisteröidytään....") // debugging...
-        await axios.post(`${this.apiUrl}/users/`, user)
+  // Sign up user and store in the database
+  async signUp(user: User): Promise<void> {
+    console.log('rekisteröidytään....'); // debugging...
+    await axios.post(`${this.apiUrl}/users/`, user);
+  }
+
+  // Sign in user and store locally
+  async signIn(user: User): Promise<void> {
+    try {
+      await AsyncStorage.setItem('email', user.email);
+      await AsyncStorage.setItem('nickname', user.nickname);
+    } catch (e: any) {
+      console.error('Error storing data', e.response?.data);
+      console.error('Status:', e.response?.status);
+      throw e;
     }
+  }
 
-    // Sign in user and store locally
-    async signIn(user: User): Promise<void> {
-        try {
-            await AsyncStorage.setItem('email', user.email)
-            await AsyncStorage.setItem('nickname', user.nickname)
-        } catch (e: any) {
-            console.error("Error storing data", e.response?.data)
-            console.error("Status:", e.response?.status)
-            throw e
-        }
-        
+  // Update user´s nickname
+  async changeNickname(nickname: string, auth0_id: string): Promise<void> {
+    try {
+      console.log('UserApiRepo ennen axios kutsua');
+      await axios.patch(`${this.apiUrl}/users/updateNickname`, {
+        nickname,
+        auth0_id,
+      });
+      console.log('UserApiRepossa: nick ja auth0: ', nickname, auth0_id);
+    } catch (e: any) {
+      console.error('changeNickname axios error', e.response?.data);
+      console.error('Status:', e.response?.status);
+      throw e;
     }
+  }
+  async getFavoriteLocations(userId: number): Promise<Location[]> {
+    const response = await axios.get(
+      `${this.apiUrl}/users/${userId}/favorite-locations`
+    );
 
-    // Update user´s nickname
-    async changeNickname(nickname: string, auth0_id: string): Promise<void> {
+    return response.data;
+  }
 
-        try{
-            console.log("UserApiRepo ennen axios kutsua")
-            await axios.patch(`${this.apiUrl}/users/updateNickname`, { nickname, auth0_id })
-            console.log("UserApiRepossa: nick ja auth0: ", nickname, auth0_id)
-        } catch (e: any) {
-            console.error("changeNickname axios error", e.response?.data)
-            console.error("Status:", e.response?.status)
-            throw e
-        }
-        
-    async getFavoriteLocations(userId: number): Promise<Location[]> {
-        const response = await axios.get(
-            `${this.apiUrl}/users/${userId}/favorite-locations`
-        );
-
-        return response.data;
+  async addFavoriteLocation(
+    userId: number,
+    location: {
+      name: string;
+      latitude: number;
+      longitude: number;
     }
-
-    async addFavoriteLocation(
-        userId: number,
-        location: {
-            name: string;
-            latitude: number;
-            longitude: number;
-        }
-    ): Promise<void> {
-        await axios.post(
-            `${this.apiUrl}/users/${userId}/favorite-locations`,
-            location
-        );
-    }
+  ): Promise<void> {
+    await axios.post(
+      `${this.apiUrl}/users/${userId}/favorite-locations`,
+      location
+    );
+  }
 }
--- src/ui/screens/ProfileScreen.tsx
+++ src/ui/screens/ProfileScreen.tsx
@@ -1,27 +1,32 @@
-
-import { ScrollView, View, Text, TouchableOpacity, ActivityIndicator, FlatList, TextInput, Modal } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+  TextInput,
+  Modal,
+} from 'react-native';
 import { styles } from '../styles/profileStyles';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../navigation/types'
+import { RootStackParamList } from '../../navigation/types';
 import GameCollectionScreen from './GameCollectionScreen';
 import { FriendCard } from '../components/FriendCard';
 import { useFriendsViewModel } from '../viewModels/useFriendsViewModel';
 import { useState, useEffect } from 'react';
 import { useProfileScreenViewModel } from '../viewModels/useProfileScreenViewModel';
 
-
-
 import Toast from 'react-native-toast-message';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ModalComponent from '../components/Modal';
 
-
 // Define navigation props for the screen
-type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>
+type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
 export default function ProfileScreen({ navigation }: Props) {
   // const vm = useProfileViewModel(() => navigation.navigate('Home'))
-  const vm = useFriendsViewModel();
+  // const vm = useFriendsViewModel();
 
   function chunkArray<T>(arr: T[], size: number): T[][] {
     const res: T[][] = [];
@@ -33,147 +38,164 @@ export default function ProfileScreen({ navigation }: Props) {
 
   // ViewModel handling profile-related business logic
   // Callbacks are used to close modal and navigate after actions
-  const vm = useProfileScreenViewModel(() => setModalVisible(false), () => navigation.navigate('Home'))
+  const vm = useProfileScreenViewModel(
+    () => setModalVisible(false),
+    () => navigation.navigate('Home')
+  );
 
   // state to control modal visibility
-  const [modalVisible, setModalVisible] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false);
 
   // state for user´s nickname
-  const [userNick, setUserNick] = useState('')
+  const [userNick, setUserNick] = useState('');
 
   // Load user´s nickname if user is logged in
   useEffect(() => {
-  if (!vm.isLoggedIn) return
+    if (!vm.isLoggedIn) return;
 
-  const loadUserNick = async () => {
-    try {
-      const nick = await AsyncStorage.getItem('nickname')
-      if (nick) {
-        setUserNick(nick)
+    const loadUserNick = async () => {
+      try {
+        const nick = await AsyncStorage.getItem('nickname');
+        if (nick) {
+          setUserNick(nick);
+        }
+      } catch (e) {
+        console.error('Failed to load nickname', e);
       }
-    } catch (e) {
-      console.error('Failed to load nickname', e)
-    }
-  }
+    };
 
-  loadUserNick()
-}, [vm.isLoggedIn])
+    loadUserNick();
+  }, [vm.isLoggedIn]);
 
   return (
     <>
-    <ScrollView
-      contentContainerStyle={styles.scrollContainer}
-      showsVerticalScrollIndicator={false}
-    >
-      {/* OTSIKKO */}
-      <View style={styles.header}>
-        <Text style={styles.title}>Oma profiili</Text>
-        <Text style={styles.subtitle}>Hei, {userNick || 'tyyppi'}!</Text>
-      </View>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        {/* OTSIKKO */}
+        <View style={styles.header}>
+          <Text style={styles.title}>Oma profiili</Text>
+          <Text style={styles.subtitle}>Hei, {userNick || 'tyyppi'}!</Text>
+        </View>
 
-      {/* ASETUKSET */}
-      <View style={styles.card}>
-        <Text style={styles.sectionTitle}>Asetukset</Text>
-        <View style={styles.settings}>
+        {/* ASETUKSET */}
+        <View style={styles.card}>
+          <Text style={styles.sectionTitle}>Asetukset</Text>
+          <View style={styles.settings}>
+            <Text style={styles.statText}>
+              Käyttäjänimi: {userNick || 'tuntematon'}
+            </Text>
+            <TouchableOpacity
+              style={styles.settingsButton}
+              onPress={() => setModalVisible(true)}
+            >
+              <Text style={styles.settingsButtonText}>Muuta</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.settings}>
+            <Text style={styles.statText}>Poista tili</Text>
+            <TouchableOpacity style={styles.deleteButton} onPress={() => {}}>
+              <Text style={styles.settingsButtonText}>Poista</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* OMAT PELIT */}
+        <View style={styles.card}>
+          <Text style={styles.sectionTitle}>Pelikokoelma</Text>
           <Text style={styles.statText}>
-            Käyttäjänimi: {userNick || 'tuntematon'}
+            Ei vielä lisättyjä pelejä - Lisää uusi peli
           </Text>
           <TouchableOpacity
-            style={styles.settingsButton}
-            onPress={() => setModalVisible(true)}>
-            <Text style={styles.settingsButtonText}>Muuta</Text>
+            style={styles.primaryButton}
+            onPress={() => navigation.navigate('GameCollection')}
+          >
+            <Text style={styles.buttonText}>Siirry pelikokoelmaan</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.settings}>
+
+        {/* PELATUT PELIT */}
+        <View style={styles.card}>
+          <Text style={styles.sectionTitle}>Pelatut pelit</Text>
           <Text style={styles.statText}>
-            Poista tili
+            Lista pelatuista peleistä? Ehkä jokin kuvaaja voitoista/häviöistä?
           </Text>
-          <TouchableOpacity style={styles.deleteButton} onPress={() => { }}>
-            <Text style={styles.settingsButtonText}>Poista</Text>
+          <TouchableOpacity
+            style={styles.primaryButton}
+            onPress={() => navigation.navigate('GameSessions')}
+          >
+            <Text style={styles.buttonText}>Näytä pelikerrat</Text>
           </TouchableOpacity>
         </View>
-      </View>
 
-      {/* OMAT PELIT */}
-      <View style={styles.card}>
-        <Text style={styles.sectionTitle}>Pelikokoelma</Text>
-        <Text style={styles.statText}>
-          Ei vielä lisättyjä pelejä - Lisää uusi peli
-        </Text>
-        <TouchableOpacity style={styles.primaryButton} onPress={() => navigation.navigate('GameCollection')}>
-          <Text style={styles.buttonText}>Siirry pelikokoelmaan</Text>
-        </TouchableOpacity>
-      </View>
+        {/* YSTÄVÄT */}
+        <View style={styles.card}>
+          <Text style={styles.sectionTitle}>Kaverit</Text>
 
-      {/* PELATUT PELIT */}
-      <View style={styles.card}>
-        <Text style={styles.sectionTitle}>Pelatut pelit</Text>
-        <Text style={styles.statText}>
-          Lista pelatuista peleistä? Ehkä jokin kuvaaja voitoista/häviöistä?
-        </Text>
-        <TouchableOpacity
-          style={styles.primaryButton}
-          onPress={() => navigation.navigate('GameSessions')}
-        >
-          <Text style={styles.buttonText}>Näytä pelikerrat</Text>
-        </TouchableOpacity>
-      </View>
+          {vm.loading ? (
+            <ActivityIndicator />
+          ) : vm.friends.length === 0 ? (
+            <Text style={styles.statText}>Ei kavereita vielä.</Text>
+          ) : (
+            chunkArray(vm.friends, 2).map((pair, idx) => (
+              <View
+                key={idx}
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'flex-start',
+                  gap: 60,
+                  marginBottom: 8,
+                }}
+              >
+                <FriendCard friend={pair[0]} />
+                {pair[1] ? (
+                  <FriendCard friend={pair[1]} />
+                ) : (
+                  <View style={{ flex: 1 }} />
+                )}
+              </View>
+            ))
+          )}
 
-      {/* YSTÄVÄT */}
-      <View style={styles.card}>
-        <Text style={styles.sectionTitle}>Kaverit</Text>
+          <TouchableOpacity
+            style={styles.primaryButton}
+            onPress={() => navigation.navigate('Friends')}
+          >
+            <Text style={styles.buttonText}>Löydä ja lisää kavereita</Text>
+          </TouchableOpacity>
+        </View>
 
-        {vm.loading ? (
-          <ActivityIndicator />
-        ) : vm.friends.length === 0 ? (
-          <Text style={styles.statText}>Ei kavereita vielä.</Text>
-        ) : (
-          chunkArray(vm.friends, 2).map((pair, idx) => (
-            <View key={idx} style={{ 
-              flexDirection: "row", 
-              justifyContent: "flex-start",
-              gap: 60,
-              marginBottom: 8}}>
-              <FriendCard friend={pair[0]} />
-              {pair[1] ? <FriendCard friend={pair[1]} /> : <View style={{ flex: 1 }} />}
-            </View>
-          ))
-        )}
+        {/* Kirjaudu ulos */}
+        <View style={styles.card}>
+          <Text style={styles.sectionTitle}>Kirjaudu ulos</Text>
 
-        <TouchableOpacity style={styles.primaryButton} onPress={() => navigation.navigate('Friends')}>
-          <Text style={styles.buttonText}>Löydä ja lisää kavereita</Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity
+            style={styles.primaryButton}
+            onPress={vm.logoutUser}
+          >
+            <Text style={styles.buttonText}>Kirjaudu ulos</Text>
+          </TouchableOpacity>
+        </View>
+        <ModalComponent
+          modalVisible={modalVisible}
+          setModalVisible={() => setModalVisible(false)}
+          header="Vaihda nimimerkki"
+          placeholder="Uusi nimimerkki"
+          inputValue={vm.nickname}
+          setInputValue={vm.setNickname}
+          checkValue={vm.checkNickname}
+          isValueAvailable={vm.isNickAvailable}
+          onPress={vm.changeNick}
+          buttonText="Vaihda"
+          showCheck={vm.showCheck}
+          trueText="Nickname on vapaa"
+          falseText="Nickname on varattu"
+        />
+      </ScrollView>
 
-      {/* Kirjaudu ulos */}
-      <View style={styles.card}>
-        <Text style={styles.sectionTitle}>Kirjaudu ulos</Text>
-
-        <TouchableOpacity style={styles.primaryButton} onPress={vm.logoutUser}>
-          <Text style={styles.buttonText}>Kirjaudu ulos</Text>
-        </TouchableOpacity>
-      </View>
-      <ModalComponent
-        modalVisible={modalVisible}
-        setModalVisible={()=>setModalVisible(false)}
-        header='Vaihda nimimerkki'
-        placeholder='Uusi nimimerkki'
-        inputValue={vm.nickname}
-        setInputValue={vm.setNickname}
-        checkValue={vm.checkNickname}
-        isValueAvailable={vm.isNickAvailable}
-        onPress={vm.changeNick}
-        buttonText='Vaihda'
-        showCheck={vm.showCheck}
-        trueText='Nickname on vapaa'
-        falseText='Nickname on varattu'
-      />
-    </ScrollView>
-
-    {/* TOAST MESSAGE */}
-      <Toast/>
+      {/* TOAST MESSAGE */}
+      <Toast />
     </>
-
-
   );
 }
